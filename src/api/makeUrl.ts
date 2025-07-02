960e42fd8707fa2aa07e403425688b2f26d24edf
Munshi Faysal <fmunshi752@gmail.com>
2025-07-03 02:47:17 +0600

    fix: update base URL to production endpoint

--- src/api/makeUrl.ts
+++ src/api/makeUrl.ts
@@ -1,6 +1,6 @@
 export function createUrl(endpoint: string): string {
   // Base URL from .env
-  const baseUrl = "https://localhost:7039/api"
+  const baseUrl = "https://sales.dermolive.com.bd/api"
   // Join base URL and endpoint safely
   return `${baseUrl.replace(/\/$/, '')}/${endpoint.replace(/^\//, '')}`;
 }
